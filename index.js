9443c8d9fb277f37bb53c0dd953ca7764f1c378b
wanyingxue94 <C13491872@mydit.ie>
2017-10-15 19:17:48 +0100

    server - setup

---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+ * Created by wanyingxue on 2017/10/14.
+ */
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const path = require('path');
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
+
+    if(err){
+        console.log('Could Not connect to database: ', err);
+    }else {
+        console.log('Connected to database: ' + config.db);
+    }
+
+});
+
+
+app.use(express.static(__dirname + '/client/dist/'));
+
+
+
+app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname + '/client/dist/index.html'));
+});
+
+app.listen(8080,() => {
+    console.log('Listening on port 8080');
+});
