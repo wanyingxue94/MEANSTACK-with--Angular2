d2c1eb9c91f0466957e984ef2bff32b07aacc138
wanyingxue94 <C13491872@mydit.ie>
2018-01-13 16:37:04 +0000

    Post Comment

--- client/src/app/components/blog/blog.component.ts
+++ client/src/app/components/blog/blog.component.ts
@@ -15,9 +15,12 @@ export class BlogComponent implements OnInit {
   newPost = false;
   loadingBlogs = false;
   form;
+  commentForm;
   processing = false;
   username;
   blogPosts;
+  newComment = [];
+  enabledComments = [];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -25,23 +28,47 @@ export class BlogComponent implements OnInit {
     private blogService: BlogService
   ) {
     this.createNewBlogForm(); // Create new blog form on start up
+    this.createCommentForm(); // Create form for posting comments on a user's blog post
   }
 
   // Function to create new blog form
   createNewBlogForm() {
     this.form = this.formBuilder.group({
+      // Title field
       title: ['', Validators.compose([
         Validators.required,
         Validators.maxLength(50),
         Validators.minLength(5),
         this.alphaNumericValidation
       ])],
+      // Body field
       body: ['', Validators.compose([
         Validators.required,
         Validators.maxLength(500),
         Validators.minLength(5)
       ])]
     })
+  }
+
+  // Create form for posting comments
+  createCommentForm() {
+    this.commentForm = this.formBuilder.group({
+      comment: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(200)
+      ])]
+    })
+  }
+
+  // Enable the comment form
+  enableCommentForm() {
+    this.commentForm.get('comment').enable(); // Enable comment field
+  }
+
+  // Disable the comment form
+  disableCommentForm() {
+    this.commentForm.get('comment').disable(); // Disable comment field
   }
 
   // Enable new blog form
@@ -75,23 +102,32 @@ export class BlogComponent implements OnInit {
   // Reload blogs on current page
   reloadBlogs() {
     this.loadingBlogs = true; // Used to lock button
-    this.getAllBlogs();
-    // Get All Blogs
+    this.getAllBlogs(); // Add any new blogs to the page
     setTimeout(() => {
       this.loadingBlogs = false; // Release button lock after four seconds
     }, 4000);
   }
 
   // Function to post a new comment on blog post
-  draftComment() {
+  draftComment(id) {
+    this.commentForm.reset(); // Reset the comment form each time users starts a new comment
+    this.newComment = []; // Clear array so only one post can be commented on at a time
+    this.newComment.push(id); // Add the post that is being commented on to the array
+  }
 
+  // Function to cancel new post transaction
+  cancelSubmission(id) {
+    const index = this.newComment.indexOf(id); // Check the index of the blog post in the array
+    this.newComment.splice(index, 1); // Remove the id from the array to cancel post submission
+    this.commentForm.reset(); // Reset  the form after cancellation
+    this.enableCommentForm(); // Enable the form after cancellation
+    this.processing = false; // Enable any buttons that were locked
   }
 
   // Function to submit a new blog post
   onBlogSubmit() {
     this.processing = true; // Disable submit button
     this.disableFormNewBlogForm(); // Lock form
-
     // Create blog object from form fields
     const blog = {
       title: this.form.get('title').value, // Title field
@@ -100,7 +136,6 @@ export class BlogComponent implements OnInit {
     }
 
     // Function to save blog into database
-
     this.blogService.newBlog(blog).subscribe(data => {
       // Check if blog was saved to database or not
       if (!data.success) {
@@ -145,12 +180,40 @@ export class BlogComponent implements OnInit {
     });
   }
 
-// Function to disliked a blog post
+  // Function to disliked a blog post
   dislikeBlog(id) {
     // Service to dislike a blog post
     this.blogService.dislikeBlog(id).subscribe(data => {
       this.getAllBlogs(); // Refresh blogs after dislike
     });
+  }
+
+  // Function to post a new comment
+  postComment(id) {
+    this.disableCommentForm(); // Disable form while saving comment to database
+    this.processing = true; // Lock buttons while saving comment to database
+    const comment = this.commentForm.get('comment').value; // Get the comment value to pass to service function
+    // Function to save the comment to the database
+    this.blogService.postComment(id, comment).subscribe(data => {
+      this.getAllBlogs(); // Refresh all blogs to reflect the new comment
+      const index = this.newComment.indexOf(id); // Get the index of the blog id to remove from array
+      this.newComment.splice(index, 1); // Remove id from the array
+      this.enableCommentForm(); // Re-enable the form
+      this.commentForm.reset(); // Reset the comment form
+      this.processing = false; // Unlock buttons on comment form
+      if (this.enabledComments.indexOf(id) < 0) this.expand(id); // Expand comments for user on comment submission
+    });
+  }
+
+  // Expand the list of comments
+  expand(id) {
+    this.enabledComments.push(id); // Add the current blog post id to array
+  }
+
+  // Collapse the list of comments
+  collapse(id) {
+    const index = this.enabledComments.indexOf(id); // Get position of id in array
+    this.enabledComments.splice(index, 1); // Remove id from array
   }
 
   ngOnInit() {
